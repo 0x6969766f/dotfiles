844545ac0bfd2a1f99fad4c6c2493e4ceb8430d4
0x6969766f <iivari.leinonen@gmail.com>
2023-05-21 00:33:29 +0300

    theming

--- bin/ohmy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- bin/p10k.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# PowerLevel10k
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-echo -e "done, restarting zsh...\n"
-exec zsh
-p10k configure
