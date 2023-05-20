b5205a1a2eefbfa1f68a93fd7a0fbcc9b06e52df
0x6969766f <iivari.leinonen@gmail.com>
2023-05-20 18:39:11 +0300

    files

---
+++ bin/install.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOTFILES=$HOME/Development/dotfiles
+
+info () {
+  printf "\r[ \033[00;34m..\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K[ \033[00;32mOK\033[0m ] $1\n"
+}
+
+info "Ask for sudo password"
+sudo -v
+
+clear
+
+# Move to ~ if we are not there yet
+cd
+
+printf "Hello $(whoami)! Let's get you set up.\n\n"
+
+# TODO: Should prompt pre-installation questions here!
+
+# Command Line Tools
+info "Installing macOS Command Line Tools..."
+if [[ ! -x /usr/bin/gcc ]]; then
+  xcode-select --install
+  success "Command Line tools installed successfully!"
+else
+  success "Command Line Tools already installed, skipping..."
+fi
+
+echo "\n"
+
+# Homebrew
+info "Installing Homebrew..."
+which -s brew
+if [[ $? != 0 ]] ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  success "Homebrew installed successfully!"
+else
+  success "Homebrew already installed, updating..." && brew update
+fi
+
+echo "\n"
+
+# Ansbible
+info "Installing Ansible..."
+if brew list ansible &> /dev/null; then
+  success "Ansible already installed! skipping..."
+else
+  brew install ansible && success "Ansible installed successfully!"
+fi
+
+echo "\n"
+
+# Dotfiles
+if [[ ! -d $DOTFILES ]]; then
+  mkdir -p $DOTFILES
+  git clone https://github.com/0x6969766f/dotfiles.git $DOTFILES
+fi
+
+cd $DOTFILES
+info "Fetching dotfiles..."
+git pull origin main
+
+# Playbook
+info "Running playbook..."
+#ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/hosts ansible/dotfiles.yml -v
+
+success "Done!"
+exit 0
+
+
